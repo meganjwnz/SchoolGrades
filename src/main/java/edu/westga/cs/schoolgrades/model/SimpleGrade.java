b9cf54b62d6c1baa4d4b77325989884982d72f80
Megan Brown <mjones29@my.westga.edu>
2018-10-24 01:17:00 -0400

    Created constructor to initialize value instance variable to but retrieved from getValue

--- src/main/java/edu/westga/cs/schoolgrades/model/SimpleGrade.java
+++ src/main/java/edu/westga/cs/schoolgrades/model/SimpleGrade.java
@@ -7,11 +7,22 @@ package edu.westga.cs.schoolgrades.model;
  * 
  */
 public class SimpleGrade implements Grade {
+	private double value;
+
+	/**
+	 * Constructor that sets the value of the grade
+	 * 
+	 * @param value Grade Value
+	 */
+	public SimpleGrade(double value) {
+		if (value > 100 || value < 0) {
+			throw new IllegalArgumentException("Grade value must be greater than 0 but less than 100.");
+		}
+		this.value = value;
+	}
 
 	@Override
 	public double getValue() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.value;
 	}
-
 }
