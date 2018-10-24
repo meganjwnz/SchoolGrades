6ad248c6180893089c0fe618b5bf84fd3914a3a4
Megan Brown <mjones29@my.westga.edu>
2018-10-24 01:21:01 -0400

    Created shell of WeightedGrade that implements Grade and its methods

---
+++ src/main/java/edu/westga/cs/schoolgrades/model/WeightedGrade.java
@@ -0,0 +1,17 @@
+package edu.westga.cs.schoolgrades.model;
+
+/**
+ * Compares the grade to the percentage of the total grade
+ * 
+ * @author Megan Brown
+ *
+ */
+public class WeightedGrade implements Grade {
+
+	@Override
+	public double getValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
