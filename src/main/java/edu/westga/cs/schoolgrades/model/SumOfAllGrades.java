76bb1111d08dd2ed348af70ed1b8ce17199a6ec9
Megan Brown <mjones29@my.westga.edu>
2018-10-24 01:32:19 -0400

    Created shell of SumOfAllGrades that implements GradeCalculatorStrategy

---
+++ src/main/java/edu/westga/cs/schoolgrades/model/SumOfAllGrades.java
@@ -0,0 +1,18 @@
+package edu.westga.cs.schoolgrades.model;
+
+import java.util.List;
+
+/**
+ * Takes all the grades of a list and adds them together
+ * @author Megan Brown
+ *
+ */
+public class SumOfAllGrades implements GradeCalculatorStrategy {
+
+	@Override
+	public double calculateGrade(List<Grade> grades) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
