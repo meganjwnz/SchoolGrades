0037266a685bb1bdcc0469f95520da192ff5ced7
Megan Brown <mjones29@my.westga.edu>
2018-10-24 01:56:43 -0400

    Created shell of DropLowestGrade that implements GradeCalculatorStrategy

---
+++ src/main/java/edu/westga/cs/schoolgrades/model/DropLowestGrade.java
@@ -0,0 +1,19 @@
+package edu.westga.cs.schoolgrades.model;
+
+import java.util.List;
+
+/**
+ * Takes all the grades and drops the lowest value grade from the list
+ * 
+ * @author Megan Brown
+ *
+ */
+public class DropLowestGrade implements GradeCalculatorStrategy {
+
+	@Override
+	public double calculateGrade(List<Grade> grades) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
