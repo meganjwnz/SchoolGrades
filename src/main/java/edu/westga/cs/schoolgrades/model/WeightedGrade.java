1ddf05510715b8df81fb748408a880a8e17001d8
Megan Brown <mjones29@my.westga.edu>
2018-10-24 01:22:21 -0400

    Created constructor to initiale grade and weight to be used in getValue

--- src/main/java/edu/westga/cs/schoolgrades/model/WeightedGrade.java
+++ src/main/java/edu/westga/cs/schoolgrades/model/WeightedGrade.java
@@ -7,11 +7,30 @@ package edu.westga.cs.schoolgrades.model;
  *
  */
 public class WeightedGrade implements Grade {
+	private Grade grade;
+	private double weight;
+
+	/**
+	 * Constructor to initialize grade and weight
+	 * 
+	 * @param grade  Grade Object
+	 * @param weight Percentage of grade
+	 * @precondition : weight must be > 0 && < 1; grade must not be null
+	 */
+	public WeightedGrade(Grade grade, double weight) {
+		if (weight < 0 || weight > 1) {
+			throw new IllegalArgumentException("Weight percentage must be between 0 and 1, inclusive.");
+		}
+		if (grade == null) {
+			throw new IllegalArgumentException("Grade must not be null.");
+		}
+		this.grade = grade;
+		this.weight = weight;
+	}
 
 	@Override
 	public double getValue() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.grade.getValue() * this.weight;
 	}
 
 }
