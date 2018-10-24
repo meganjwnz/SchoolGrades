431ac8b0a5d56a0674a991438f3472078fac3596
Megan Brown <mjones29@my.westga.edu>
2018-10-24 01:28:59 -0400

    Created GradeCalculatorStrategy interface

---
+++ src/main/java/edu/westga/cs/schoolgrades/model/GradeCalculatorStrategy.java
@@ -0,0 +1,13 @@
+package edu.westga.cs.schoolgrades.model;
+
+import java.util.List;
+
+/**
+ * GradeCalculatorStrategy to enforce calculations
+ * @author Megan Brown
+ *
+ */
+public interface GradeCalculatorStrategy {
+
+	public double calculateGrade(List<Grade> grades);
+}
