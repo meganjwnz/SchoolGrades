448f47d3d5fa40ba2f27b8f90413ce090fe8d25a
Megan Brown <mjones29@my.westga.edu>
2018-10-24 01:15:03 -0400

    Created shell of SimpleGrade class that implements Grade

---
+++ src/main/java/edu/westga/cs/schoolgrades/model/SimpleGrade.java
@@ -0,0 +1,17 @@
+package edu.westga.cs.schoolgrades.model;
+
+/**
+ * Sets up a simple grade object, who's grade value can be set and retrieved
+ * 
+ * @author Megan Brown
+ * 
+ */
+public class SimpleGrade implements Grade {
+
+	@Override
+	public double getValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
