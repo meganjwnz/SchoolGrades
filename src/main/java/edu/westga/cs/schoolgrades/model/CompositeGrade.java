4a17607ddc0af6c8a554a5c806255b7bb413502e
Megan Brown <mjones29@my.westga.edu>
2018-10-24 01:37:55 -0400

    Returned to CompositeGrade to create Constructor that initializes arraylist and GradeCalculatorStrategy. Added functionallity to getValue to calculate the value of all the grades based on the GradeCalculatorStrategy

--- src/main/java/edu/westga/cs/schoolgrades/model/CompositeGrade.java
+++ src/main/java/edu/westga/cs/schoolgrades/model/CompositeGrade.java
@@ -1,16 +1,32 @@
 package edu.westga.cs.schoolgrades.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Joins all the grades together in an arraylist 
  * @author Megan Brown
  *
  */
 public class CompositeGrade implements Grade {
-
+	private List<Grade> grades;
+	private GradeCalculatorStrategy gcs;
+	
+	/**
+	 * Constructor to initialize GradeCalculatorStrategy and Grade ArrayList
+	 * @param gcs
+	 */
+	public CompositeGrade(GradeCalculatorStrategy gcs) {
+		if (gcs == null) {
+			throw new IllegalArgumentException ("GradeCalculatorStrategy must not be null");
+		}
+		this.grades = new ArrayList<Grade>();
+		this.gcs = gcs;
+	}
+	
 	@Override
 	public double getValue() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.gcs.calculateGrade(this.grades);
 	}
 
 }
