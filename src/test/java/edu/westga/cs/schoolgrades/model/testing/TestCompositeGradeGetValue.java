3bcaf82dd0cfa837868c4f28e23a972abd2faf46
Megan Brown <mjones29@my.westga.edu>
2018-10-24 01:55:25 -0400

    Added JUnit testing to getValue of CompositeGrade to use AverageOfAllGrades

--- src/test/java/edu/westga/cs/schoolgrades/model/testing/TestCompositeGradeGetValue.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/testing/TestCompositeGradeGetValue.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import edu.westga.cs.schoolgrades.model.AverageOfAllGrades;
 import edu.westga.cs.schoolgrades.model.CompositeGrade;
 import edu.westga.cs.schoolgrades.model.GradeCalculatorStrategy;
 import edu.westga.cs.schoolgrades.model.SimpleGrade;
@@ -29,6 +30,26 @@ class TestCompositeGradeGetValue {
 		GradeCalculatorStrategy gcs = new SumOfAllGrades();
 		CompositeGrade compGrade = new CompositeGrade(gcs);
 		assertEquals(0, compGrade.getValue());
+	}
+	
+	@Test
+	void testGetValueWithOf3GradesUsingAverageOfAllGrades() {
+		GradeCalculatorStrategy gcs = new AverageOfAllGrades();
+		CompositeGrade compGrade = new CompositeGrade(gcs);
+		SimpleGrade s1 = new SimpleGrade(90.0);
+		SimpleGrade s2 = new SimpleGrade(50.0);
+		SimpleGrade s3 = new SimpleGrade(40.0);
+		compGrade.addGrade(s1);
+		compGrade.addGrade(s2);
+		compGrade.addGrade(s3);
+		assertEquals(60, compGrade.getValue());
+	}
+
+	@Test
+	void testGetValueWithOfNoGradesUsingAverageOfAllGrades() {
+		GradeCalculatorStrategy gcs = new AverageOfAllGrades();
+		CompositeGrade compGrade = new CompositeGrade(gcs);
+		assertEquals(0.0, compGrade.getValue());
 	}
 
 }
