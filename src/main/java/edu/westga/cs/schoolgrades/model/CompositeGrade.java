8ad04008935a5b75f61b8d77e46f6c0b66064771
Megan Brown <mjones29@my.westga.edu>
2018-10-24 01:28:06 -0400

    Created shell of CompositeGrade. GradeCalculatorStrategy required to continue.

---
+++ src/main/java/edu/westga/cs/schoolgrades/model/CompositeGrade.java
@@ -0,0 +1,16 @@
+package edu.westga.cs.schoolgrades.model;
+
+/**
+ * Joins all the grades together in an arraylist 
+ * @author Megan Brown
+ *
+ */
+public class CompositeGrade implements Grade {
+
+	@Override
+	public double getValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
