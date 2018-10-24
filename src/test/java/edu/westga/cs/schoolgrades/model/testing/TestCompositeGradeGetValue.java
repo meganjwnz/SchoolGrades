63112397a16c3358bf6b96545ae9f1a0585733a5
Megan Brown <mjones29@my.westga.edu>
2018-10-24 02:02:36 -0400

    Added JUnit testing to getValue of CompositeGrade to use DropLowestGrade

--- src/test/java/edu/westga/cs/schoolgrades/model/testing/TestCompositeGradeGetValue.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/testing/TestCompositeGradeGetValue.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import edu.westga.cs.schoolgrades.model.AverageOfAllGrades;
 import edu.westga.cs.schoolgrades.model.CompositeGrade;
+import edu.westga.cs.schoolgrades.model.DropLowestGrade;
 import edu.westga.cs.schoolgrades.model.GradeCalculatorStrategy;
 import edu.westga.cs.schoolgrades.model.SimpleGrade;
 import edu.westga.cs.schoolgrades.model.SumOfAllGrades;
@@ -48,6 +49,28 @@ class TestCompositeGradeGetValue {
 	@Test
 	void testGetValueWithOfNoGradesUsingAverageOfAllGrades() {
 		GradeCalculatorStrategy gcs = new AverageOfAllGrades();
+		CompositeGrade compGrade = new CompositeGrade(gcs);
+		assertEquals(0.0, compGrade.getValue());
+	}
+	
+	@Test
+	void testGetValueWithOf3GradesUsingDropLowestGrade() {
+		GradeCalculatorStrategy averageCalc = new AverageOfAllGrades();
+		GradeCalculatorStrategy gcs = new DropLowestGrade(averageCalc);
+		CompositeGrade compGrade = new CompositeGrade(gcs);
+		SimpleGrade s1 = new SimpleGrade(80.0);
+		SimpleGrade s2 = new SimpleGrade(70.0);
+		SimpleGrade s3 = new SimpleGrade(30.0);
+		compGrade.addGrade(s1);
+		compGrade.addGrade(s2);
+		compGrade.addGrade(s3);
+		assertEquals(75, compGrade.getValue());
+	}
+
+	@Test
+	void testGetValueWithOfNoGradesUsingDropLowestGrade() {
+		GradeCalculatorStrategy averageCalc = new AverageOfAllGrades();
+		GradeCalculatorStrategy gcs = new DropLowestGrade(averageCalc);
 		CompositeGrade compGrade = new CompositeGrade(gcs);
 		assertEquals(0.0, compGrade.getValue());
 	}
