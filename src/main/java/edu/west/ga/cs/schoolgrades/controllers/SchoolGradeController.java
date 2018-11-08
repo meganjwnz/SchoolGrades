0a4a87dd32585a611aa8d8d675291d4f54e49760
Megan Brown <mjones29@my.westga.edu>
2018-11-08 00:56:48 -0500

    Added and initialized observable lists using simplegrades

--- src/main/java/edu/west/ga/cs/schoolgrades/controllers/SchoolGradeController.java
+++ src/main/java/edu/west/ga/cs/schoolgrades/controllers/SchoolGradeController.java
@@ -1,6 +1,9 @@
 package edu.west.ga.cs.schoolgrades.controllers;
 
 import edu.westga.cs.schoolgrades.model.SimpleGrade;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
@@ -31,4 +34,17 @@ public class SchoolGradeController {
 
 	@FXML
 	private Button recalculate;
+	
+	private ObservableList<SimpleGrade> quizzes;
+	private ObservableList<SimpleGrade> homeworks;
+	private ObservableList<SimpleGrade> exams;
+	
+	/**
+	 * Initalizes instance variables
+	 */
+	public SchoolGradeController() {
+		this.quizzes = FXCollections.observableArrayList();
+		this.homeworks = FXCollections.observableArrayList();
+		this.exams = FXCollections.observableArrayList();
+	}
 }
