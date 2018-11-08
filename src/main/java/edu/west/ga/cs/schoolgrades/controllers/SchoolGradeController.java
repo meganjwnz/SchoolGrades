22f181fb7f84dd8fea422ea4577ddd6238b2dd88
Megan Brown <mjones29@my.westga.edu>
2018-11-08 00:58:02 -0500

    Created cellfactory based of Babble project to display values onto listview in GUI

--- src/main/java/edu/west/ga/cs/schoolgrades/controllers/SchoolGradeController.java
+++ src/main/java/edu/west/ga/cs/schoolgrades/controllers/SchoolGradeController.java
@@ -6,8 +6,12 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.TextFieldListCell;
+import javafx.util.Callback;
+import javafx.util.StringConverter;
 
 public class SchoolGradeController {
 	
@@ -46,5 +50,32 @@ public class SchoolGradeController {
 		this.quizzes = FXCollections.observableArrayList();
 		this.homeworks = FXCollections.observableArrayList();
 		this.exams = FXCollections.observableArrayList();
+	}
+	
+	/**
+	 * Manages the cell factory of the grades. Converts grades into string so that
+	 * they can be displayed in the list pane. Based of Babble cell factory
+	 * 
+	 * @author Megan Brown
+	 *
+	 */
+	private class GradeManager implements Callback<ListView<SimpleGrade>, ListCell<SimpleGrade>> {
+		public ListCell<SimpleGrade> call(ListView<SimpleGrade> listView) {
+			TextFieldListCell<SimpleGrade> cellFactory = new TextFieldListCell<>();
+			cellFactory.setConverter(new StringConverter<SimpleGrade>() {
+				@Override
+				public String toString(final SimpleGrade grade) {
+					final String gradeResult = grade.getValue() + "";
+					cellFactory.setAccessibleText(gradeResult);
+					return gradeResult;
+				}
+
+				@Override
+				public SimpleGrade fromString(final String string) {
+					return null;
+				}
+			});
+			return cellFactory;
+		}
 	}
 }
