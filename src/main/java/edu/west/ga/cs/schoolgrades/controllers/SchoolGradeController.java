5940ea4df68d995f2ad7c08ce3d1b24ccd892ac0
Megan Brown <mjones29@my.westga.edu>
2018-11-08 00:55:16 -0500

    Connected @FXML objects to Controller

--- src/main/java/edu/west/ga/cs/schoolgrades/controllers/SchoolGradeController.java
+++ src/main/java/edu/west/ga/cs/schoolgrades/controllers/SchoolGradeController.java
@@ -1,5 +1,34 @@
 package edu.west.ga.cs.schoolgrades.controllers;
 
-public class SchoolGradeController {
+import edu.westga.cs.schoolgrades.model.SimpleGrade;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
+public class SchoolGradeController {
+	
+	@FXML
+	private TextField quizzesSubtotal;
+
+	@FXML
+	private TextField homeworksSubtotal;
+
+	@FXML
+	private TextField examsSubtotal;
+
+	@FXML
+	private TextField finalGradeTotal;
+
+	@FXML
+	private ListView<SimpleGrade> quizGrades;
+
+	@FXML
+	private ListView<SimpleGrade> homeworkGrades;
+
+	@FXML
+	private ListView<SimpleGrade> examGrades;
+
+	@FXML
+	private Button recalculate;
 }
