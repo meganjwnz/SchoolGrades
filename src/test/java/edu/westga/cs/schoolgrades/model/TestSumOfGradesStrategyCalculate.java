2e1fc48c84a717ca14a85e70f783aebbeac0ed55
Megan Brown <mjones29@my.westga.edu>
2018-12-03 00:50:09 -0500

    Created mock objects of TestSumOfGradesStrategyCalculate

--- src/test/java/edu/westga/cs/schoolgrades/model/TestSumOfGradesStrategyCalculate.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestSumOfGradesStrategyCalculate.java
@@ -4,7 +4,7 @@ package edu.westga.cs.schoolgrades.model;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,9 +12,9 @@ import org.junit.jupiter.api.Test;
 public class TestSumOfGradesStrategyCalculate {
 
 	private static final double DELTA = 0.001;
-	private Grade grade0;
-	private Grade grade1;
-	private Grade grade2;
+	private Grade mockGrade0;
+	private Grade mockGrade1;
+	private Grade mockGrade2;
 	
 	private List<Grade> grades;
 	
@@ -22,9 +22,12 @@ public class TestSumOfGradesStrategyCalculate {
 	
 	@BeforeEach
 	public void setup() {
-		grade0 = new SimpleGrade(10);
-		grade1 = new SimpleGrade(20);
-		grade2 = new SimpleGrade(30);
+		mockGrade0 = mock(SimpleGrade.class);
+		when(mockGrade0.getValue()).thenReturn(10.0);
+		mockGrade1 = mock(SimpleGrade.class);
+		when(mockGrade1.getValue()).thenReturn(20.0);
+		mockGrade2 = mock(SimpleGrade.class);
+		when(mockGrade2.getValue()).thenReturn(30.0);
 		
 		grades = new ArrayList<Grade>();
 		
@@ -45,15 +48,15 @@ public class TestSumOfGradesStrategyCalculate {
 	
 	@Test
 	public void shouldCalculateSumOfOneGrades() {
-		grades.add(grade0);
-		assertEquals(grade0.getValue(), strategy.calculate(grades), DELTA);
+		grades.add(mockGrade0);
+		assertEquals(mockGrade0.getValue(), strategy.calculate(grades), DELTA);
 	}
 
 	@Test
 	public void shouldCalculateSumOManyGrades() {
-		grades.add(grade0);
-		grades.add(grade1);
-		grades.add(grade2);
+		grades.add(mockGrade0);
+		grades.add(mockGrade1);
+		grades.add(mockGrade2);
 		assertEquals(60, strategy.calculate(grades), DELTA);
 	}
 }
